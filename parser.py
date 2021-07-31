03a1ee64e33a3a2b025688406e084bf56125e570
Vili Lähtevänoja <vili.lahtevanoja@gmail.com>
2021-07-31 20:13:13 +0300

    Initial commit

---
+++ parser.py
@@ -0,0 +1,26 @@
+import shapefile
+from pyproj import Transformer
+
+csv_data = ["Id,Address,Longitude,Latitude\n"]
+
+# from ETRS-GK25 (EPSG:3879) offered by HSY data, to EPSG:4326 also known as WGS84 which is accepted by Google Maps
+coord_transformer = Transformer.from_crs("epsg:3879", "epsg:4326", always_xy=True)
+
+with shapefile.Reader("HSY_Vesipostit_shp/HSY_Vesipostit", encoding="latin") as sf:
+    assert sf.shapeType == shapefile.POINT
+    shapeRecs = sf.shapeRecords()
+    # print(shapeRecs[0].shape.points)
+    for shapeRec in shapeRecs:
+        id = shapeRec.record[0]
+        address = shapeRec.record[1]
+        lon_utm = shapeRec.record[2]
+        lat_utm = shapeRec.record[3]
+        lat, lon = coord_transformer.transform(lat_utm, lon_utm)
+        # print(shapeRec.record)
+        line = f"{id},{address},{lat},{lon}\n"
+        csv_data.append(line)
+
+print(csv_data)
+
+with open("output.csv", "w") as f:
+    f.writelines(csv_data)
